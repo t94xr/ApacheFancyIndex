0202f8f829f94d220e767bd2df4c9a31993fe654
Cameron Walker <t94xrnz@gmail.com>
2025-04-07 22:40:33 +1200

    Added Footer Link to Github Project

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-
+<a href="//github.com/t94xr/ApacheFancyIndex/">Apache Fancy Index</a>
 <span style="float: right;"><script src="https://www.w3counter.com/tracker.js?id=151404"></script>
 <img src="https://img.shields.io/badge/Debian-A81D33?style=for-the-badge&amp;logo=debian&amp;logoColor=white"/>
 </span>
