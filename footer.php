1a5326c42ef6b0d53834d81ca758fb7814f11c80
Cameron Walker <t94xrnz@gmail.com>
2025-04-07 22:45:01 +1200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,11 +1,6 @@
 <footer>
 <a href="//github.com/t94xr/ApacheFancyIndex/">Apache Fancy Index</a>
-<span style="float: right;"><script src="https://www.w3counter.com/tracker.js?id=151404"></script>
-<img src="https://img.shields.io/badge/Debian-A81D33?style=for-the-badge&amp;logo=debian&amp;logoColor=white"/>
-</span>
 </footer>
-
-<p class="disclaimer"><strong>Disclaimer:</strong> Files accessable on this server may cause data loss if used improperly.</p>
 
 </div>
 </body>
